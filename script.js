daeb0f79b7ed2d8543d070cf2f1c595e622cf130
Ellis Pothos <184855785+ellis-pothos@users.noreply.github.com>
2024-12-03 07:30:43 -0500

    clear sketchpad added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // main "container" holds both the
 // button container div and the boxes container div 
 const container = document.querySelector("#container");
-container.classList.add("content"); 
+container.classList.add("mainContainer"); 
 
 // container to contain the input fields 
 const inputContainer = document.createElement("div");
@@ -33,61 +33,53 @@ container.appendChild(inputContainer);
 
         // telling the computer when to run function how many
         btn.addEventListener(`click`, () => {
+            clearSketch();
             const userRows = rowsInput.value;
             const userColumns = columnsInput.value; 
             // console.log("TEsting");
-            // clearSketch();
             newPad(userRows, userColumns);
         });
     
 // functions for clearing the old sketch
 // replace the box container with a new box container
-// function clearSketch() {
-    // const oldBoxContainer = document.querySelector("#boxContainer");
-    // const newBoxContainer = document.createElement("div");
-    // oldBoxContainer.replaceWith(newBoxContainer);
-
-    // append the new box container to container 
-    // newBoxContainer.classList.add("boxContainer"); 
-    // container.appendChild(newBoxContainer);
-// } 
+function clearSketch() {
+        const oldBoxContainer = document.getElementById("boxContainer");
+        oldBoxContainer.remove(); 
+} 
 
 // telling the computer what to do when the submit button is clicked 
 function newPad(userRows, userColumns) {
+    const boxContainer = document.createElement("div");
+    boxContainer.id = "boxContainer"; 
+    
+    // append the new box container to container 
+    boxContainer.classList.add("boxContainer"); 
+    container.appendChild(boxContainer);
+
         // create new rows and columns according to user input
     // console.log("testing newpad")
     for (let r = 0; r < userRows; r++) {
         // newRow();
-        // function newRow() {
-            //function for creating divs
-            const originalRow = document.getElementById("row");
-            const clonedRow = originalRow.cloneNode(true);
-            boxContainer.appendChild(clonedRow); 
-        // };
+        const row = document.createElement("div");
+        row.id = "row";
+        row.classList.add("row"); 
+
+        for (let c = 0; c < userColumns; c++) {
+            const box = document.createElement("div");
+            box.id = "box";
+            box.classList.add("boxes");
+            row.appendChild(box);
+        }
+        boxContainer.appendChild(row); 
         // console.log("testing rows");
     }
-    for (let c = 0; c < userColumns; c++) {
-        // newCol();
-        // function newCol() {
-            //function for creating divs
-            const originalCol = document.getElementById("box");
-            const clonedCol = originalCol.cloneNode(true);
-            boxContainer.appendChild(clonedCol); 
-        // };
-        // console.log("testing columns");
-    }
 }
+
 
 const boxContainer = document.createElement("div");
 boxContainer.id = "boxContainer";
 boxContainer.classList.add("boxContainer");
 container.appendChild(boxContainer);
 
-
-
-const box1a = document.createElement("div");
-box1a.id = "box"
-box1a.classList.add("boxes");
-row1.appendChild(box1a);
 
     
